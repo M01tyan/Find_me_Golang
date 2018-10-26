18f9f9a0d3fae6755c2d5bc02f5516febf1f603a
M01tyan <kanta01m.tyan@gmail.com>
2018-10-27 01:04:23 +0900

    Client Edits Base

--- client/src/Edits.js
+++ client/src/Edits.js
@@ -4,22 +4,10 @@ import axios from 'axios'
 import AppBar from '@material-ui/core/AppBar'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import DeleteIcon from '@material-ui/icons/Delete'
 import './Edits.css'
-
-const ranges = [
-  {
-    value: '男性',
-    label: 'Male  (男性)',
-  },
-  {
-    value: '女性',
-    label: 'Female   (女性)',
-  },
-  {
-    value: 'その他',
-    label: 'Other   (その他)',
-  },
-];
 
 export default class Edits extends Component {
   constructor(props) {
@@ -37,7 +25,7 @@ export default class Edits extends Component {
       <div className="edits">
         <h2 className="title">編集フォーム</h2>
         <AppBar position="static">
-          <Tabs value={value} onChange={this.handleChange}>
+          <Tabs value={value} onChange={this.handleChange} scrollable scrollButtons="auto">
             <Tab label="基本情報" />
             <Tab label="これまでの制作物" />
             <Tab label="プログラミングレベル" />
@@ -61,31 +49,65 @@ class BaseEdit extends Component {
     super(props)
     this.state = {
       base: {
-        name: "",
-        furigana: "",
-        university: "",
-        department: "",
-        subject: "",
+        name: '',
+        furigana: '',
+        university: '',
+        department: '',
+        subject: '',
         liked: '',
-        sites: [],
+        sites: [{url_title: "github", url: "https://github.com/M01tyan"},],
         graduate: '',
         todo_id: '',
         visitor_id: '',
       },
+      new_site: {
+        url_title: '',
+        url: '',
+      },
     };
   }
   handleChange = name => event => {
-    this.setState({...this.state.base, [name]: event.target.value});
+    let base = this.state.base
+    base[name] = event.target.value
+    this.setState({base: base})
   };
-  
+  handleSitesChange = (name, id) => event => {
+    let base = this.state.base
+    base.sites[id][name] = event.target.value
+    this.setState({base: base})
+  }
+  handleNewSiteChange = name => event => {
+    let new_site = this.state.new_site
+    new_site[name] = event.target.value
+    this.setState({new_site: new_site})
+  }
+  deleteSite = id => event => {
+    console.log(id)
+    let base = this.state.base
+    base.sites.splice(id, 1)
+    this.setState({base: base})
+  }
   handleSubmit = event => {
     //event.preventDefalut();
     console.log("form ok")
     this.handleMessageSubmit({name: this.state.name, furigana: this.state.furigana, university: this.state.university, department: this.state.department, subject: this.state.subject, graduate_year: this.state.graduate_year, email: this.state.email, tel: this.state.tel, github: this.state.github, facebook: this.state.facebook, twitter: this.state.twitter})
     console.log("form success")
   }
-  handleMessageSubmit(message) {
-    console.log(message)
+  pushNewSite = event => {
+    let base = this.state.base
+    base.sites.push(this.state.new_site)
+    this.setState({base: base})
+    this.setState({new_site: { url_title: '', url: ''}})
+  }
+  onKeyPress = event => {
+    if (event.charCode === 13) { // enter key pressed
+      // do something here
+      event.preventDefault();
+      this.pushNewSite()
+    } 
+  }
+  handleMessageSubmit = event => {
+    console.log(this.state.base)
     /*
     $.ajax({
         url: "/bases/1",
@@ -102,6 +124,7 @@ class BaseEdit extends Component {
     */
   }
   componentDidMount() {
+    /*
     axios
       .get("http://localhost:8000/api/users", {
         headers: {
@@ -110,22 +133,6 @@ class BaseEdit extends Component {
       .then(results => {
         const message = results.data
         this.setState({ base: message })
-        //console.log(typeof this.state.base.name)
-      })
-      
-      /*
-      $.ajax({
-        url: "http://localhost:5000/api/users",
-        dataType: 'json',
-        cache: false,
-        success: function(message) {
-          console.log(message)
-          //this.setState({ name: message.name, furigana: message.furigana, university: message.university, department: message.department, subject: message.subject, graduate_year: message.graduate_year, email: message.email, tel: message.tel, github: message.github, facebook: message.facebook, twitter: message.twitter })
-          console.log("success")
-        }.bind(this),
-        error: function(_xhr, status, err) {
-          console.error(this.props.url, status, err.toString())
-        }.bind(this)
       })
       */
   }
@@ -133,175 +140,137 @@ class BaseEdit extends Component {
   render() {
     return (
       <div className="edit">
-        <form className="edit-form" noValidate autoComplete="off" onSubmit={this.handleMessageSubmit}>
-          <TextField
-            required
-            id="standard-required"
-            label="Name"
-            className="edit-form-text-field"
-            type="search"
-            value={this.state.base.name}
-            onChange={this.handleChange('name')}
-            margin="normal"
-          />
-          
-          <TextField
-            id="standard-normal"
-            label="Furigana(ふりがな)"
-            className="edit-form-text-field"
-            type="search"
-            value={this.state.base.furigana}
-            onChange={this.handleChange('furigana')}
-            margin="normal"
-          />
-          <TextField
-            required
-            id="standard-required"
-            label="University"
-            type="search"
-            value={this.state.base.university}
-            onChange={this.handleChange('university')}
-            className="edit-form-text-field"
-            margin="normal"
-          />
-          <TextField
-            required
-            id="standard-required"
-            label="Department(学部)"
-            type="search"
-            value={this.state.base.department}
-            onChange={this.handleChange('department')}
-            className="edit-form-text-field"
-            margin="normal"
-          />
-          <TextField
-            required
-            id="standard-required"
-            label="subject(学科)"
-            type="search"
-            value={this.state.base.subject}
-            onChange={this.handleChange('subject')}
-            className="edit-form-text-field"
-            margin="normal"
-          />
-          <TextField
-            required
-            id="standard-required"
-            label="Graduate Year(卒業年度)"
-            type="search"
-            value={this.state.base.graduate}
-            onChange={this.handleChange('graduate')}
-            className="edit-form-text-field"
-            margin="normal"
-          />
-          {this.state.base.sites.map((site, i) => (
-            <div>
+        <div className="edit-form">
+          <div className="edit-form-base">
+            <TextField
+              required
+              id="standard-required"
+              label="Name(名前)"
+              className="edit-form-text-field"
+              type="search"
+              value={this.state.base.name}
+              onChange={this.handleChange('name')}
+              margin="normal"
+            />
+            
+            <TextField
+              id="standard-normal"
+              label="Furigana(ふりがな)"
+              className="edit-form-text-field"
+              type="search"
+              value={this.state.base.furigana}
+              onChange={this.handleChange('furigana')}
+              margin="normal"
+            />
+            <TextField
+              required
+              id="standard-required"
+              label="University(大学)"
+              type="search"
+              value={this.state.base.university}
+              onChange={this.handleChange('university')}
+              className="edit-form-text-field"
+              margin="normal"
+            />
+            <TextField
+              required
+              id="standard-required"
+              label="Department(学部)"
+              type="search"
+              value={this.state.base.department}
+              onChange={this.handleChange('department')}
+              className="edit-form-text-field"
+              margin="normal"
+            />
+            <TextField
+              required
+              id="standard-required"
+              label="subject(学科)"
+              type="search"
+              value={this.state.base.subject}
+              onChange={this.handleChange('subject')}
+              className="edit-form-text-field"
+              margin="normal"
+            />
+            <TextField
+              required
+              id="standard-required"
+              label="Graduate Year(卒業年度)"
+              type="search"
+              value={this.state.base.graduate}
+              onChange={this.handleChange('graduate')}
+              className="edit-form-text-field"
+              margin="normal"
+            />
+          </div>
+          <div className="edit-form-sites">
+            {this.state.base.sites.map((site, i) => (
+              <div className="edit-form-site" key={i}>
+                <TextField
+                  id="standard-required"
+                  label="Site Title"
+                  type="search"
+                  value={site.url_title}
+                  onChange={this.handleSitesChange('url_title', i)}
+                  className="edit-form-url-title"
+                  margin="normal"
+                />
+                <TextField
+                  id="standard-required"
+                  label="Site URL"
+                  type="search"
+                  placeholder="https://www.facebook.com/"
+                  value={site.url}
+                  fullWidth
+                  onChange={this.handleSitesChange('url', i)}
+                  margin="normal"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                <IconButton aria-label="Delete" onClick={this.deleteSite(i)}>
+                  <DeleteIcon />
+                </IconButton>
+              </div>
+            ))}
+            <div className="edit-form-site">
               <TextField
                 id="standard-required"
-                label="Site Title"
+                label="New Site Title"
                 type="search"
-                value={site.url_title}
-                onChange={this.handleChange('url_title')}
-                className="edit-form-text-field"
+                value={this.state.new_site.url_title}
+                onChange={this.handleNewSiteChange('url_title')}
+                className="edit-form-url-title"
                 margin="normal"
               />
               <TextField
                 id="standard-required"
-                label="Facebook Link"
+                label="New Site URL"
                 type="search"
                 placeholder="https://www.facebook.com/"
-                value={site.url}
-                onChange={this.handleChange('facebook')}
+                value={this.state.new_site.url}
+                fullWidth
+                onChange={this.handleNewSiteChange('url')}
                 margin="normal"
                 InputLabelProps={{
                   shrink: true,
                 }}
+                onKeyPress={this.onKeyPress}
               />
             </div>
-          ))}
-          {/*
-          <TextField
-            select
-            className="edit-form-text-field"
-            variant="outlined"
-            label="Gender(性別)"
-            value={this.state.image}
-            onChange={this.handleChange('gender')}
-          >
-            {ranges.map(option => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
-          <TextField
-            id="standard-full-width"
-            label="Facebook Link"
-            type="search"
-            style={{ margin: 8 }}
-            placeholder="https://www.facebook.com/"
-            fullWidth
-            value={this.state.base.facebook}
-            onChange={this.handleChange('facebook')}
-            margin="normal"
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-          <TextField
-            id="standard-full-width"
-            label="Github Link"
-            type="search"
-            style={{ margin: 8 }}
-            placeholder="https://github.com/"
-            fullWidth
-            value={this.state.base.github}
-            onChange={this.handleChange('github')}
-            margin="normal"
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-          <TextField
-            id="standard-full-width"
-            label="Twitter Link"
-            type="search"
-            style={{ margin: 8 }}
-            placeholder="https://twitter.com/"
-            fullWidth
-            value={this.state.base.twitter}
-            onChange={this.handleChange('twitter')}
-            margin="normal"
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-          <TextField
-            id="standard-textarea"
-            label="With placeholder multiline"
-            placeholder="Placeholder"
-            multiline
-            className="edit-form-text-field"
-            margin="normal"
-          />
-        */}
-          <TextField
-            id="standard-bare"
-            className="edit-form-text-field"
-            defaultValue="Bare"
-            margin="normal"
-          />
-          <input type="submit" value="Update Post" onClick={this.handleSubmit} />
-        </form>
+          </div>
+          <div className="edit-form-post-button">
+            <Button variant="outlined" color="primary" onClick={this.handleMessageSubmit}>
+              POST
+            </Button>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
 class TodoEdit extends Component {
-  componentDidMount() {
-    console.log("Hello")
-  }
   render() {
     return (
       <h1>Hello!</h1>
